77d7c1911b0a8579ecacf0a38d27c1fddecf271a
MrUnwonted <arjungodzarjun@gmail.com>
2023-01-07 14:20:34 +0530

    Succesfull authorization

--- src/main/java/com/example/demo/AdminSecurityConfig.java
+++ src/main/java/com/example/demo/AdminSecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -19,16 +19,29 @@ public class AdminSecurityConfig {
 	}
 	
 	@Bean
-	protected PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
-	}
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 	
 	@Bean
 	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		
-		http.authorizeHttpRequests().requestMatchers("/**").permitAll();
-		return http.build();
+		http.authorizeHttpRequests().requestMatchers("/").permitAll();
 		
-		http.requestMatcher("/admin/**").authorizeRequests().anyRequest().hasAuthority("ADMIN");
+		 http.authorizeHttpRequests((requests) -> requests
+     			.requestMatchers("/admin/**").hasAnyRole("ADMIN")
+     			.anyRequest().authenticated())
+         .formLogin()
+             .loginPage("/admin/login")
+             .usernameParameter("email")
+             .loginProcessingUrl("/admin/login")
+             .defaultSuccessUrl("/admin/home")
+             .permitAll()
+         .and()
+         .logout()
+             .logoutUrl("/admin/logout")
+             .logoutSuccessUrl("/");
+		return http.build();    
+ } 
 	}
-}
+
